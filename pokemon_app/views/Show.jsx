349e05c66e7eb651b64292648ebb3bd0723b00a1
littlelionstudios <flavorformulas@gmail.com>
2023-04-16 00:33:13 -0400

    Created show views of each pokemon

--- pokemon_app/views/Show.jsx
+++ pokemon_app/views/Show.jsx
@@ -2,8 +2,14 @@ const React = require('react')
 
 class Show extends React.Component {
     render () {
+        const { pokemon } = this.props;
         return (
-            <h1> Show Page </h1>
+            <div>
+                <h1>Gotta Catch 'Em All</h1>
+                <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
+                <img src={pokemon.img + ".jpg"} />
+                <a href={"/pokemon"}>back</a>
+            </div>
         );
     }
 }
