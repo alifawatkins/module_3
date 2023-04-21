f0ca6f98f815fc5ec9d454c439a5226c0a5fa069
littlelionstudios <flavorformulas@gmail.com>
2023-04-21 04:39:29 -0400

    third day

--- mern-infrastructure/src/App.js
+++ mern-infrastructure/src/App.js
@@ -1,10 +1,28 @@
+import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {Routes, Route} from 'react-router-dom'
+import AuthPage from './pages/AuthPage';
+import NewOrderPage from './pages/NewOrderPage';
+import OrderHistoryPage from './pages/OrderHistoryPage';
+import NavBar from './components/NavBar';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
-    <div className="App">
-      <h1>SEI Cafe</h1>
-    </div>
+    <main className="App">
+      { user ?
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+        :
+        <AuthPage />
+      }
+    </main>
   );
 }
 
