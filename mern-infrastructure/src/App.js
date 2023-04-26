b41ac928cdc7da76c284eee2ae1fb3845cb92568
littlelionstudios <flavorformulas@gmail.com>
2023-04-25 23:26:52 -0400

    caught up

--- mern-infrastructure/src/App.js
+++ mern-infrastructure/src/App.js
@@ -1,26 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState} from 'react';
-import {Routes, Route} from 'react-router-dom'
-import AuthPage from './pages/AuthPage';
+import {useState} from 'react';
+
+import { Routes, Route } from 'react-router-dom';
+
 import NewOrderPage from './pages/NewOrderPage';
+import AuthPage from './pages/AuthPage';
 import OrderHistoryPage from './pages/OrderHistoryPage';
 import NavBar from './components/NavBar';
 
+import { getUser } from './utilities/users-service';
+
+import './App.css';
+
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(getUser());
+
   return (
     <main className="App">
-      { user ?
-        <>
-          <NavBar />
-          <Routes>
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
-          </Routes>
-        </>
-        :
-        <AuthPage />
+     { user ? 
+      <>
+      <NavBar user={user} setUser={setUser}/>
+      <Routes>
+        <Route path='/orders/new' element={ <NewOrderPage /> }/>
+        <Route path='/orders' element={ <OrderHistoryPage /> }/>
+      </Routes>
+      </>
+     : 
+      <AuthPage setUser={setUser}/>
       }
     </main>
   );
--- mern-infrastructure/src/pages/AuthPage.js
+++ mern-infrastructure/src/pages/AuthPage.js
@@ -1,12 +1,26 @@
-import SignUpForm from "../components/SignUpForm";
+import { useState } from "react";
 
-function AuthPage() {
-    return (
-        <main>
-            <h1>AuthPage</h1>
-            <SignUpForm />
-        </main>
-    )
+import SignUpForm from "../components/SignUpForm";
+import LoginForm from "../components/LogInForm";
+
+function AuthPage({ setUser }) {
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <main className="AuthPage">
+      <h1>Auth Page</h1>
+
+      <button onClick={() => setShowLogin(!showLogin)}>
+        {showLogin ? "Sign up" : "Sign in"}
+      </button>
+
+      {showLogin ? (
+        <LoginForm setUser={setUser} />
+      ) : (
+        <SignUpForm setUser={setUser} />
+      )}
+    </main>
+  );
 }
 
 export default AuthPage;
