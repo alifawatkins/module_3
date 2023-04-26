3b04f11e5d9ae08697018c18b35cdda00b1a924d
littlelionstudios <flavorformulas@gmail.com>
2023-04-25 23:26:52 -0400

    caught up

--- mern-infrastructure/controllers/api/users.js
+++ mern-infrastructure/controllers/api/users.js
@@ -1,32 +1,58 @@
+//* Request handler Logic
 const User = require('../../models/user');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+//* /*-- Helper Functions --*/
+function createJWT(user) {
+    return jwt.sign({user}, process.env.SECRET, {expiresIn: '24h'});
+}
+
+async function create(req, res) {
+    // console.log('[From POST handler]', req.body)
+    try {
+        //* creating a new user
+        const user = await User.create(req.body);
+        console.log(user);
+
+        //* creating a new jwt
+        const token = createJWT(user);
+
+        res.json(token);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error)
+    }
+}
+
+
+async function login(req, res) {
+    try {
+        // find user in db
+      const user = await User.findOne({ email: req.body.email });
+      // check if we found an user
+      if (!user) throw new Error();
+      // compare the password to hashed password
+      const match = await bcrypt.compare(req.body.password, user.password);
+      // check is password matched
+      if (!match) throw new Error();
+      // send back a new token with the user data in the payload
+      res.json( createJWT(user) );
+    } catch {
+      res.status(400).json('Bad Credentials');
+    }
+  }
+
+
+async function checkToken(req, res) {
+    console.log(req.user);
+    res.json(req.exp)
+}
+
 
 module.exports = {
-    create
-};
-
-/*-- Helper Functions --*/
-
-function createJWT(user) {
-    return jwt.sign(
-      // data payload
-      { user },
-      process.env.SECRET,
-      { expiresIn: '24h' }
-    );
-}
-async function create(req, res) {
-    try {
-        // Add the user to the database
-        const user = await User.create(req.body);
-        // token will be a string
-        const token = createJWT(user);
-        // Yes, we can use res.json to send back just a string
-        // The client code needs to take this into consideration
-        res.json(token);
-    } catch (err) {
-        // Client will check for non-2xx status code
-        // 400 = Bad Request
-        res.status(400).json(err);
-    }
+    create,
+    login,
+    checkToken
 }
--- mern-infrastructure/server.js
+++ mern-infrastructure/server.js
@@ -1,36 +1,42 @@
+require('dotenv').config();
+require('./config/database'); // connects to db
 const express = require('express');
-const path = require('path');
+const path = require('path'); // node module
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
-require('dotenv').config();
-
-// Connect to the database
-require('./config/database');
-
 const app = express();
+// development port: 3001
+// in production we'll a PORT number set in the environment variables
+const PORT = process.env.PORT || 3001;
 
+
+
+//* Config
+// Logger middleware
 app.use(logger('dev'));
+// JSON payload middleware (for data coming from frontend functions)
 app.use(express.json());
-
 // Configure both serve-favicon & static middleware
 // to serve from the production 'build' folder
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+// checks if token was sent and sets a user data on the req (req.user)
+app.use(require('./config/checkToken'));
 
-// Put API routes here, before the "catch all" route
+// * All other routes
 app.use('/api/users', require('./routes/api/users'));
 
+
+
+// Put API routes here, before the "catch all" route
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
-// Configure to use port 3001 instead of 3000 during
-// development to avoid collision with React's dev server
-const port = process.env.PORT || 3001;
 
-app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
-});
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})
