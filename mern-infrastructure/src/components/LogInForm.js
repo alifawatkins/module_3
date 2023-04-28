9123cd747d8134cf8f9a3b231bb441895b624adb
littlelionstudios <flavorformulas@gmail.com>
2023-04-27 21:30:38 -0400

    more updates wouldya believe it

--- mern-infrastructure/src/components/LogInForm.js
+++ mern-infrastructure/src/components/LogInForm.js
@@ -1,5 +1,3 @@
-// LoginForm.jsx
-
 import { useState } from 'react';
 import * as usersService from '../utilities/users-service';
 
@@ -30,17 +28,17 @@ export default function LoginForm({ setUser }) {
     }
 
     return (
-    <div>
-        <div className="form-container" onSubmit={handleSubmit}>
-        <form autoComplete="off" >
-            <label>Email</label>
-            <input type="text" name="email" value={credentials.email} onChange={handleChange} required />
-            <label>Password</label>
-            <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
-            <button type="submit">LOG IN</button>
-        </form>
+        <div>
+            <div className="form-container">
+            <form autoComplete="off" onSubmit={handleSubmit}>
+                <label>Email</label>
+                <input type="text" name="email" value={credentials.email} onChange={handleChange} required />
+                <label>Password</label>
+                <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
+                <button type="submit">LOG IN</button>
+            </form>
+            </div>
+            <p className="error-message">&nbsp;{error}</p>
         </div>
-        <p className="error-message">&nbsp;{error}</p>
-    </div>
     );
 }
--- mern-infrastructure/src/components/SignUpForm.js
+++ mern-infrastructure/src/components/SignUpForm.js
@@ -1,64 +1,62 @@
-import { useState } from "react";
-import {signUp} from '../utilities/users-service';
+import { Component } from "react";
+import { signUp } from '../utilities/users-service';
 
-function SignUpForm({setUser}) {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    confirm: "",
-    error: "",
-  });
+export default class SignUpForm extends Component {
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        confirm: '',
+        error: ''
+    };
 
-  const disable = formData.password !== formData.confirm;
+    handleChange = (evt) => {
+        this.setState({
+            [evt.target.name]: evt.target.value,
+            error: ''
+        });
+    };
 
-  const handleSubmit = async (e) => { 
-    e.preventDefault(); 
-    
-    try {
-      console.log(formData)
-      // data to be send to the backend to create a new user
-      const userData = {
-        name: formData.name,
-        email: formData.email,
-        password: formData.password
-      }
-      // returns a token with the user info
-      const user = await signUp(userData); // user service
-      setUser(user);
+    handleSubmit = async (evt) => {
+        evt.preventDefault();
+        try {
+            const formData = {...this.state};
+            delete formData.confirm;
+            delete formData.error;
+            // The promise returned by the signUp service method
+            // will resolve to the user object included in the
+            // payload of the JSON Web Token (JWT)
+            const user = await signUp(formData);
+            // Baby step
+            this.props.setUser(user);
+        } catch {
+            // An error happened on the server
+            this.setState({ error: 'Sign Up Failed - Try Again' });
+        }
+    };
 
-    } catch (error) {
-      setFormData({...formData, error: "Sign Up Failed - Try Again"})
+    // We must override the render method
+    // The render method is the equivalent to a function-based component
+    // (its job is to return the UI)
+    render() {
+        const disable = this.state.password !== this.state.confirm;
+        return (
+            <div>
+                <div className="form-container">
+                    <form autoComplete="off" onSubmit={this.handleSubmit}>
+                        <label>Name</label>
+                        <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+                        <label>Email</label>
+                        <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+                        <label>Password</label>
+                        <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+                        <label>Confirm</label>
+                        <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+                        <button type="submit" disabled={disable}>SIGN UP</button>
+                    </form>
+                </div>
+                <p className="error-message">&nbsp;{this.state.error}</p>
+            </div>
+        );
     }
-};
-
-  const handleChange = (evt) => {
-    setFormData({...formData, [evt.target.name]: evt.target.value, error: ''})
-  };
-
-  return (
-    <div>
-      <div className="form-container">
-        <form autoComplete="off" onSubmit={handleSubmit}>
-            <label>Name</label>
-            <input type="text" name="name" value={formData.name} onChange={handleChange} required/>
-            
-            <label>Email</label>
-            <input type="text" name="email" value={formData.email} onChange={handleChange} required/>
-            
-            <label>password</label>
-            <input type="password" name="password" value={formData.password} onChange={handleChange} required/>
-            
-            <label>Confirm</label>
-            <input type="password" name="confirm" value={formData.confirm} onChange={handleChange} required/>
-
-            <button type="submit" disabled={disable}>SIGN UP</button>
-        </form>
-      </div>
-
-      <p className="error-message">{formData.error}</p>
-    </div>
-  );
 }
-
-export default SignUpForm;
