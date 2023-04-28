c7ef2668b3b007514bcbc9d07b8d5e8c3e00e589
littlelionstudios <flavorformulas@gmail.com>
2023-04-27 21:48:05 -0400

    would you look at that

--- mern-infrastructure/server.js
+++ mern-infrastructure/server.js
@@ -1,42 +1,44 @@
-require('dotenv').config();
-require('./config/database'); // connects to db
 const express = require('express');
-const path = require('path'); // node module
+const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
+require('dotenv').config();
+
+// Connect to the database
+require('./config/database');
+
 const app = express();
-// development port: 3001
-// in production we'll a PORT number set in the environment variables
-const PORT = process.env.PORT || 3001;
 
-
-
-//* Config
-// Logger middleware
 app.use(logger('dev'));
-// JSON payload middleware (for data coming from frontend functions)
 app.use(express.json());
+
 // Configure both serve-favicon & static middleware
 // to serve from the production 'build' folder
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
-// checks if token was sent and sets a user data on the req (req.user)
+
+// Middleware to verify token and assign user object of payload to req.user.
+// Be sure to mount before routes
 app.use(require('./config/checkToken'));
 
-// * All other routes
-app.use('/api/users', require('./routes/api/users'));
-
-
-
 // Put API routes here, before the "catch all" route
+app.use('/api/users', require('./routes/api/users'));
+// Protect the API routes below from anonymous users
+const ensureLoggedIn = require('./config/ensureLoggedIn');
+app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
+app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'));
+
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+const port = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-})
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
--- mern-infrastructure/src/utilities/users-service.js
+++ mern-infrastructure/src/utilities/users-service.js
@@ -1,60 +1,41 @@
-// * We will use a src/utilities/users-service.js module to organize functions used to sign-up, log in, log out, etc.
+import * as usersAPI  from './users-api';
 
-//* SignUpForm.jsx <--> users-service.js <--> users-api.js <-Internet-> server.js (Express)
-
-//* handleSubmit <--> [signUp]-users-service <--> [signUp]-users-api <-Internet-> server.js (Express)
-
-import * as usersApi from './users-api';
-
-//* Get Token
-export function getToken() {
-    const token = localStorage.getItem('token');
-    // if there is no token
-    if (!token) return null;
-
-    const payload = JSON.parse(atob(token.split('.')[1]));
-    console.log(payload);
-
-    // if token is expired
-    if (payload.exp < Date.now() / 1000) {
-        localStorage.removeItem('token');
-        return null;
-    }
-
-    // token is valid
-    return token;
-
-}
-
-//* Get User
-export function getUser() {
-    const token = getToken();
-    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
-}
-
-//* SignUp
 export async function signUp(userData) {
-     // Delegate the network request code to the users-api.js API module
-    // which will ultimately return a JSON Web Token (JWT)
-    // console.log('[From SignUP function]', userData);
-    const token = await usersApi.signUp(userData);
-    // saves token to localStorage
-    localStorage.setItem('token', token);
-    
-    return getUser();
-}
-
-//* LogOut
-export function logOut() {
-    localStorage.removeItem('token')
+  // Delete the network request code to the
+  // users-api.js module which will ultimately
+  // return the JWT
+  const token = await usersAPI.signUp(userData);
+  // Persist the token to localStorage
+  localStorage.setItem('token', token);
+  return getUser();
 }
 
 export async function login(credentials) {
-    const token = await usersApi.login(credentials)
-    localStorage.setItem('token', token);
-    return getUser();
+  const token = await usersAPI.login(credentials);
+  // Persist the token to localStorage
+  localStorage.setItem('token', token);
+  return getUser();
 }
 
-export async function checkToken() {
-    return usersApi.checkToken().then(dateStr => new Date(dateStr))
+export function getToken() {
+  const token = localStorage.getItem('token');
+  // getItem will return null if no key
+  if (!token) return null;
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  // A JWT's expiration is expressed in seconds, not miliseconds
+  if (payload.exp < Date.now() / 1000) {
+    // Token has expired
+    localStorage.removeItem('token');
+    return null;
+  }
+  return token;
+}
+
+export function getUser() {
+  const token = getToken();
+  return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+}
+
+export function logOut() {
+  localStorage.removeItem('token');
 }
