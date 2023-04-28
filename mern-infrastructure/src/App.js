294818260051777633b800a9a3108152be3e94c9
littlelionstudios <flavorformulas@gmail.com>
2023-04-27 21:25:16 -0400

    updated

--- mern-infrastructure/src/App.js
+++ mern-infrastructure/src/App.js
@@ -1,31 +1,25 @@
-import {useState} from 'react';
-
-import { Routes, Route } from 'react-router-dom';
-
-import NewOrderPage from './pages/NewOrderPage';
+import React, {useState} from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom'
 import AuthPage from './pages/AuthPage';
+import NewOrderPage from './pages/NewOrderPage';
 import OrderHistoryPage from './pages/OrderHistoryPage';
-import NavBar from './components/NavBar';
-
 import { getUser } from './utilities/users-service';
-
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   const [user, setUser] = useState(getUser());
-
   return (
-    <main className="App">
-     { user ? 
-      <>
-      <NavBar user={user} setUser={setUser}/>
-      <Routes>
-        <Route path='/orders/new' element={ <NewOrderPage /> }/>
-        <Route path='/orders' element={ <OrderHistoryPage /> }/>
-      </Routes>
-      </>
-     : 
-      <AuthPage setUser={setUser}/>
+    <main className={styles.App}>
+      { user ?
+        <>
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
+            <Route path="/orders" element={<OrderHistoryPage user={user} setUser={setUser} />} />
+            <Route path="/*" element={<Navigate to="/orders/new" />} />
+          </Routes>
+        </>
+        :
+        <AuthPage setUser={setUser}/>
       }
     </main>
   );
